4d683217227bc72cd58120e6387ea01e3cf4ab0c
KD13n3r <kevin.diener@younglings.africa>
2024-01-30 14:20:45 +0200

    completed design of messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+      <div className='messageInfo'>
+        <img 
+        src='https://images.pexels.com/photos/19951472/pexels-photo-19951472/free-photo-of-buck-in-a-snowstorm.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' 
+        alt=''
+        /> 
+        <span>just now</span>
+      </div>
+      <div className='messageContent'>
+        <p>hello</p>
+        <img src='https://images.pexels.com/photos/19951472/pexels-photo-19951472/free-photo-of-buck-in-a-snowstorm.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt=''/>
+      </div>
+    </div>
   )
 }
 
