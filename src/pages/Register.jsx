6351ac2f61a803256006fe6178d985f4957d70a8
KD13n3r <kevin.diener@younglings.africa>
2024-01-31 10:12:24 +0200

    error handling in register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,28 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Add from "../img/addAvatar.png"
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 
 const Register = () => {
-
-  const handleSubmit = (e) => {
+  const [err,setErr]= useState(false)
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
     const file = e.target[3].files[0];
 
-  createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
+    try{
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      
+    }catch(err){
+      setErr(true);
+    }
+    
+
   }
 
   return (
@@ -40,6 +37,7 @@ const Register = () => {
             <span>Add an avatar</span>
           </label>
           <button>Sign up</button>
+          {err && <span>Something went wrong</span>}
         </form>
         <p>You do have an account? Login</p>
       </div>
