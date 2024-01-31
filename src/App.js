631ad6e1cf635b095d5e546550a910acc42248a6
KD13n3r <kevin.diener@younglings.africa>
2024-01-31 14:47:12 +0200

    adding auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { useContext } from "react";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import "./style.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
   return (
     <BrowserRouter>
       <Routes>
