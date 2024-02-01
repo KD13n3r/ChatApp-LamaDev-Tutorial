5ee95351b5e22197fa003a966d3254a9163c5cdd
KD13n3r <kevin.diener@younglings.africa>
2024-02-01 14:40:59 +0200

    search functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,53 @@
-import React from 'react'
+import React, { useState } from "react";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Search = () => {
-  return (
-    <div className='search'>
-      <div className='searchForm'>
-        <input type='text' placeholder='Find a user'/>
-      </div>
-      <div className='userChat'>
-        <img src='https://images.pexels.com/photos/19951472/pexels-photo-19951472/free-photo-of-buck-in-a-snowstorm.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt=''/>
-        <div className='userChatInfo'>
-          <span>Jane</span>
-        </div>
-      </div>
-    </div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
 
-export default Search
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("dispalyName", "==", username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input
+          type="text"
+          placeholder="Find a user"
+          onKeyDown={handleKey}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      {err && <span>User not found!</span>}
+      {user && <div className="userChat">
+        <img
+          src={user.photoURL}
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>{user.displayName}</span>
+        </div>
+      </div>}
+    </div>
+  );
+};
+
+export default Search;
