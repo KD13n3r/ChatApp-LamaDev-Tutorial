cc12a08d26396e7ad29be52529cab9d0274f815a
KD13n3r <kevin.diener@younglings.africa>
2024-02-01 14:57:40 +0200

    check whether the group(chats in firestore) exists, if not create

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Search = () => {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
 
   const handleSearch = async () => {
     const q = query(
@@ -26,6 +29,16 @@ const Search = () => {
   const handleKey = (e) => {
     e.code === "Enter" && handleSearch();
   };
+
+  const handleSelect = async () => {
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const res = await getDocs(db, "chats", combinedId);
+    } catch (err) {}
+  };
   return (
     <div className="search">
       <div className="searchForm">
@@ -37,15 +50,14 @@ const Search = () => {
         />
       </div>
       {err && <span>User not found!</span>}
-      {user && <div className="userChat">
-        <img
-          src={user.photoURL}
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>{user.displayName}</span>
+      {user && (
+        <div className="userChat" onClick={handleSelect}>
+          <img src={user.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
         </div>
-      </div>}
+      )}
     </div>
   );
 };
