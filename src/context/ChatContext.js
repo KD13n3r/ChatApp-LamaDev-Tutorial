12f0e06e9f5e085a9bc167a771d473b7bb18113d
KD13n3r <kevin.diener@younglings.africa>
2024-02-05 14:56:05 +0200

    handle select

--- src/context/ChatContext.js
+++ src/context/ChatContext.js
@@ -1,12 +1,4 @@
-import { onAuthStateChanged } from "firebase/auth";
-import {
-  createContext,
-  useContext,
-  useEffect,
-  useReducer,
-  useState,
-} from "react";
-import { auth } from "../firebase";
+import { createContext, useContext, useReducer } from "react";
 import { AuthContext } from "./AuthContext";
 
 export const ChatContext = createContext();
@@ -37,7 +29,7 @@ export const ChatContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(chatReducer, INITIAL_STATE);
 
   return (
-    <ChatContext.Provider value={{ data:state, dispatch }}>
+    <ChatContext.Provider value={{ data: state, dispatch }}>
       {children}
     </ChatContext.Provider>
   );
