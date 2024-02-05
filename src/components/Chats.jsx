4eb520d34ab70da7adc3ce30fbd54f018126466b
KD13n3r <kevin.diener@younglings.africa>
2024-02-05 11:55:42 +0200

    show currnet user's current chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,31 +1,62 @@
-import React from 'react'
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Chats = () => {
+  const [chats, setChats] = useState([]);
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+  
+      return () => {
+        unsub();
+      };
+    }
+
+    currentUser.uid && getChats()
+  }, [currentUser.uid]);
+
+
   return (
-    <div className='chats'>
-      <div className='userChat'>
-        <img src='https://images.pexels.com/photos/19951472/pexels-photo-19951472/free-photo-of-buck-in-a-snowstorm.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt=''/>
-        <div className='userChatInfo'>
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/19951472/pexels-photo-19951472/free-photo-of-buck-in-a-snowstorm.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <div className="userChatInfo">
           <span>Jane</span>
           <p>Hello</p>
         </div>
       </div>
-      <div className='userChat'>
-        <img src='https://images.pexels.com/photos/19951472/pexels-photo-19951472/free-photo-of-buck-in-a-snowstorm.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt=''/>
-        <div className='userChatInfo'>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/19951472/pexels-photo-19951472/free-photo-of-buck-in-a-snowstorm.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <div className="userChatInfo">
           <span>Jane</span>
           <p>Hello</p>
         </div>
       </div>
-      <div className='userChat'>
-        <img src='https://images.pexels.com/photos/19951472/pexels-photo-19951472/free-photo-of-buck-in-a-snowstorm.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt=''/>
-        <div className='userChatInfo'>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/19951472/pexels-photo-19951472/free-photo-of-buck-in-a-snowstorm.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <div className="userChatInfo">
           <span>Jane</span>
           <p>Hello</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chats
+export default Chats;
