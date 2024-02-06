f226f89e73aa931548ecb98ccd8be26ccec0ca72
KD13n3r <kevin.diener@younglings.africa>
2024-02-06 13:35:52 +0200

    input messages

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,21 +1,50 @@
-import React from 'react'
-import Img from "../img/img.png"
-import Attach from "../img/attach.png"
+import React, { useContext, useState } from "react";
+import Img from "../img/img.png";
+import Attach from "../img/attach.png";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Input = () => {
+  const [text, setText] = useState("");
+  const [img, setImg] = useState(null);
+
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const handleSend = async () => {
+    if (img) {
+    } else {
+      await updateDoc(doc(db, "chats", data.chatId),{
+        messages: arrayUnion({
+          id:
+        })
+      });
+    }
+  };
   return (
-    <div className='input'>
-      <input type='text' placeholder='Type something...'/>
-      <div className='send'>
-        <img src={Attach} alt=''/>
-        <input type='file' style={{display:"none"}} id='file'/>
-        <label htmlFor='file'>
-          <img src={Img} alt=''/>
+    <div className="input">
+      <input
+        type="text"
+        placeholder="Type something..."
+        onChange={(e) => setText(e.target.value)}
+      />
+      <div className="send">
+        <img src={Attach} alt="" />
+        <input
+          type="file"
+          style={{ display: "none" }}
+          id="file"
+          onChange={(e) => setImg(e.target.file[0])}
+        />
+        <label htmlFor="file">
+          <img src={Img} alt="" />
         </label>
-        <button>Send</button>
+        <button onClick={handleSend}>Send</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
